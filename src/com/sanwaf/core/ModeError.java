56d469ba31f3eb0db54ff0db2f308eb50c07d7a7
Sterling Fullerton <sterlingfullerton@me.com>
2026-02-26 00:02:53 -0500

    Convert ModeError singleton from class to enum

--- src/com/sanwaf/core/ModeError.java
+++ src/com/sanwaf/core/ModeError.java
@@ -1,8 +1,7 @@
 package com.sanwaf.core;
 
 //used to return non-null/null (non-null = stop validation, null = continue)
-class ModeError
+enum ModeError
 {
-  static final ModeError INSTANCE = new ModeError();
+  INSTANCE;
 }
-
